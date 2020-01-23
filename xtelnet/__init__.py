432e835b7be00985442948a61524f9e57a9291a7
AlaBouali <trap.leader.123@gmail.com>
2020-01-23 23:43:28 +0100

    Add files via upload

--- xtelnet/__init__.py
+++ xtelnet/__init__.py
@@ -1,1 +1,115 @@
-
+import telnetlib,socket,re
+def escape_ansi(line):
+    return  re.compile(r'(?:\x1B[@-Z\\-_]|[\x80-\x9A\x9C-\x9F]|(?:\x1B\[|\x9B)[0-?]*[ -/]*[@-~])').sub('',line.decode("utf-8"))
+class session:
+ def __init__(self):
+  self.prompt=''
+  self.prompt_end=b''
+  self.telnet=None
+  self.logs={}#where we save input and output logs
+ def login(self,u,username=None,password=None,p=23,timeout=3):
+  try:
+   usr=False
+   pwd=False
+   self.telnet = telnetlib.Telnet(u,p,timeout=timeout)
+   while True:
+    m=self.telnet.expect([b'ser:',b'Name:',b'sername:',b'name:',b'ogin:',b'assword:',b'Pass:',b'pass:',b'nter>'],timeout=timeout)#expected login prompts
+    s=m[2]
+    s=escape_ansi(s)
+    if (('name:' in str(s).lower()) or ('login:' in str(s).lower()) or ('user:' in str(s).lower())):#in case it asked for username
+     if usr==True:
+        raise Exception("Authentication Failed")#so we don't get tricked into sending username multiple times after failure
+     self.telnet.write("{}\n".format(username).encode('utf-8'))#send username
+     usr=True
+    elif (("password:" in str(s).lower()) or ("pass:" in str(s).lower())):#in case it asked for password
+     self.telnet.write("{}\n".format(password).encode('utf-8'))#send password
+     pwd=True
+     break
+    elif "enter>" in str(s).lower():
+        self.telnet.write("\n".format(cmd.strip(),new_line).encode('utf-8'))#some anti-bot techniques requires sending "enter" after sending username/password
+    else:
+      c=str(s).lower()
+      c=c.replace("b'",'')
+      c=c.replace("'",'')
+      c=c.replace('b"','')
+      c=c.replace('"','')
+      c=c.strip()
+      if ((c[-1:]=='$') or (c[-1:]=='#') or (c[-1:]=='%') or (c[-1:]=='>') or (c[-1:]==']') ):#in case this is unauthenticated server
+       self.prompt=(c.split("\r\n")[-1]).strip()
+       if (c[-1:]=='$'):
+           self.prompt_end=b'$'
+       if (c[-1:]=='>'):
+           self.prompt_end=b'>'
+       if (c[-1:]=='#'):
+           self.prompt_end=b'#'
+       if (c[-1:]=='%'):
+           self.prompt_end=b'%'
+       if (c[-1:]==']'):
+           self.prompt_end=b']'
+       return None
+      break
+   while True:
+      c=escape_ansi(self.telnet.read_some())#keep reading the data till get the login result
+      c=c.replace("b'",'')
+      c=c.replace("'",'')
+      c=c.replace('b"','')
+      c=c.replace('"','')
+      c=c.strip()
+      if ('enter>' in c):
+          self.telnet.write("\n".format(cmd.strip(),new_line).encode('utf-8'))
+      else:
+       if (('denied' in c) or ('bad' in c) or ("incorrect" in c) or ('failed' in c) or ('wrong' in c) or ('invalid' in c) or ('name:' in c) or ('login:' in c) or ('user:' in c) or ('password:' in c) or  ('pass:' in c)):
+          raise Exception("Authentication Failed")#if login failed
+       if ((c[-1:]=='$') or (c[-1:]=='#') or (c[-1:]=='%') or (c[-1:]=='>') or (c[-1:]==']') ):#in case this is unauthenticated server
+        self.prompt=(c.split("\r\n")[-1]).strip()
+        if (c[-1:]=='$'):
+           self.prompt_end=b'$'
+        if (c[-1:]=='>'):
+           self.prompt_end=b'>'
+        if (c[-1:]=='#'):
+           self.prompt_end=b'#'
+        if (c[-1:]=='%'):
+           self.prompt_end=b'%'
+        if (c[-1:]==']'):
+           self.prompt_end=b']'
+        return None
+  except socket.timeout:
+   raise Exception("Timed out")
+ def execute(self,cmd,new_line='\n',timeout=2,more_timeout=2):#this function executes any command and returns the output
+    if cmd.strip()!='':
+     self.telnet.write("{} {}".format(cmd.strip(),new_line).encode('utf-8'))#send the command
+     x=str(self.prompt)
+     c=self.telnet.read_until(b"{}".format(self.prompt),timeout=timeout)#read data until it receive the end of the prompt after executing the command
+     if "---- More ----" in str(c):
+         while True:
+             self.telnet.write("\n".format(cmd.strip(),new_line).encode('utf-8'))
+             o=self.telnet.read_until(b"---- More ----",timeout=more_timeout)
+             if x.lower() in str(c).lower():
+              break
+             c+=o
+     c=escape_ansi(c)
+     c=c.replace("b'",'')
+     c=c.replace("'",'')
+     c=c.replace('b"','')
+     c=c.replace('"','')
+     c=c.strip()
+     try:
+      c=cmd.strip().join(c.split(cmd.strip())[1:]).strip()#remove the command sent from output
+     except:
+      pass
+     try:
+      self.prompt=(c.split("\r\n")[-1]).strip()#update telnet prompt when changing directory or terminal type
+     except:
+      pass
+     try:
+      c="\r\n".join(c.split("\r\n")[:-1]).strip()#remove the prompt from output
+     except:
+      pass
+     self.logs.update({str(cmd.strip()):str(c.strip())})#save the input and output into dict as logs
+     return c.strip()
+ def close(self):
+     self.telnet.close()#close telnet connection
+ def logout(self,timeout=1):
+     self.execute("logout",timeout=timeout)#logout of the telnet session
+ def exit(self,timeout=1):
+     self.execute("exit",timeout=timeout)#exit the telnet session
---
+++ xtelnet/__main__.py
@@ -0,0 +1,95 @@
+import sys
+if  sys.version_info < (3,0):
+    input=raw_input
+from xtelnet.__init__ import *
+c=sys.argv
+user=""
+pwd=""
+host=''
+port=23
+timeout=5
+commands=[]
+command_timeout=5
+new_line="\n"
+shell=True
+usage="""
+
+usage: python -m xtelnet host [options...]
+
+options:
+
+--username : set a username (required if username is needed to access)
+--password : set a password (required if password is needed to access)
+--port : (23 by default) set port
+--timeout : (5 by default) set timeout
+--add-command : a command to execute after login and disable shell
+--command-timeout : (5 by default) timeout for command execution
+--set-newline : ("\\n" by default) set a new line indecator("\\n" or "\\r\\n")
+--no-shell : (enabled by default if no commands are specified) disable shell after authentication
+--help : get this help message
+
+example:
+
+python -m xtelnet 127.0.0.1 --username root --password root --add-command "echo ala" --add-command "dir"
+
+"""
+if len(c)<2:
+    print(usage)
+    sys.exit()
+host=c[1]
+i=0
+while(i<(len(c))):
+    x=c[i]
+    if (x=="--help"):
+        print(usage)
+        sys.exit()
+    if (x=="--port"):
+        port=int(c[i+1])
+        i+=1
+    if (x=="--timeout"):
+        timeout=int(c[i+1])
+        i+=1
+    if (x=="--username"):
+        user=c[i+1]
+        i+=1
+    if (x=="--no-shell"):
+        shell=False
+    if (x=="--password"):
+        pwd=c[i+1]
+        i+=1
+    if (x=="--add-command"):
+     commands.append(c[i+1])
+     i+=1
+    if (x=="--command-timeout"):
+        command_timeout=c[i+1]
+        i+=1
+    if (x=="--set-newline"):
+        command_timeout=c[i+1]
+        i+=1
+    i+=1
+if host=='':
+    print("You need to set a host to connect to!!!")
+    sys.exit()
+if len(commands)>0:
+    shell=False
+t=session()
+try:
+ t.login(host,username=user,password=pwd,timeout=timeout,p=port)
+ if len(commands)>0:
+  for x in commands:
+     print(t.prompt+str(x))
+     print(str(t.execute(x,timeout=command_timeout,new_line=new_line)))
+  t.close()
+ if shell==True:
+  while True:
+      cmd=input(t.prompt)
+      if ((cmd.lower().strip()=="exit") or (cmd.lower().strip()=="logout")):
+          t.close()
+          sys.exit()
+      output=t.execute(cmd,timeout=command_timeout,new_line=new_line)
+      if output==None:
+        output=''
+      print(output)
+ t.close()
+except Exception as e:
+     print("[-]Error: "+str(e))
