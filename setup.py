ba73248c960802450efa9c98cee37ed3dc4309f5
AlaBouali <trap.leader.123@gmail.com>
2020-02-18 02:28:47 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name="xtelnet",
-    version="1.1.7",
+    version="1.1.8",
     author="AlaBouali",
     author_email="trap.leader.123@gmail.com",
     description="simple telnet module",
