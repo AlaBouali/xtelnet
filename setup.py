813f2d1d7913571bffe7ad2559251f368e444909
AlaBouali <trap.leader.123@gmail.com>
2021-02-04 17:05:13 -0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name="xtelnet",
-    version="2.1.0",
+    version="2.1.1",
     author="AlaBouali",
     author_email="trap.leader.123@gmail.com",
     description="simple telnet module",
